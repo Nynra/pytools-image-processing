f30eea9168fb9548350d37903d603bc01a25002d
nynra <nynradev@pm.me>
2024-03-12 22:08:52 +0100

    Renamed the package

--- setup.py
+++ setup.py
@@ -3,21 +3,18 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r") as fh:
+    install_requires = fh.read().splitlines()
+
 setuptools.setup(
-    name="pyimageprocessing",
+    name="pytools_image_processing",
     version="0.0.2",
     author="Nynra",
     author_email="nynradev@pm.me",
     description="Some usefull functions for image processing and analysis.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    py_modules=["hhslablib"],
+    py_modules=["pytools_image_processing"],
     package_dir={"": "src"},
-    install_requires=[
-        "numpy",
-        "opencv-python",
-        "matplotlib",
-        "scikit-image",
-        "scipy",
-    ],
+    install_requires=install_requires,
 )
