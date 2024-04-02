2cea8dad4307da7d29e2fa510730f78287781c2a
nynra <nynradev@pm.me>
2024-04-02 19:06:56 +0200

    Updated version number

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r") as fh:
-    install_requires = fh.read().splitlines()
+# with open("requirements.txt", "r") as fh:
+#     install_requires = fh.read().splitlines()
 
 setuptools.setup(
     name="pytools_image_processing",
-    version="0.0.2",
+    version="0.0.3",
     author="Nynra",
     author_email="nynradev@pm.me",
     description="Some usefull functions for image processing and analysis.",
@@ -16,5 +16,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     py_modules=["pytools_image_processing"],
     package_dir={"": "src"},
-    install_requires=install_requires,
+    install_requires=[
+        "opencv-python",
+        "scikit-image",
+        "matplotlib",
+        "scipy",
+        "numpy",
+    ]
 )
