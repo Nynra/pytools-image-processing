ede0e67c6560011d26928b0901e73b039af5d289
Nynra <nynradev@pm.me>
2025-02-25 19:39:47 +0100

    Updated the install method

--- setup.py
+++ setup.py
@@ -15,24 +15,14 @@ install_requires = [
     "numpy",
 ]
 
-# Get the absolute path to the directory containing setup.py
-here = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(here, "src/pytools_image_processing/__about__.py"), "r") as f:
-    about = {}
-    exec(f.read(), about)
-
-
 setuptools.setup(
     include_package_data=True,
-    name=about["__title__"],
-    version=about["__version__"],
-    author=about["__author__"],
-    author_email=about["__author_email__"],
-    description=about["__description__"],
-    long_description="long_description",
-    long_description_content_type="text/markdown",
-    py_modules=[about["__title__"]],
+    name="pytools_image_processing",
+    version="0.0.5",
+    author="Nynra",
+    author_email="nynradev@pm.me",
+    description="Some usefull functions for image processing and analysis.",
+    py_modules=["pytools_image_processing"],
     package_dir={"": "src"},
     install_requires=install_requires,
 )
